51c5bb2dc29ff28e47ca5b62c7c27cae0449d393
aethelind <aethelind@gmail.com>
2019-12-19 13:10:18 -0500

    day01 part2

--- day01/solution01.rb
+++ day01/solution01.rb
@@ -1,6 +1,24 @@
+# part one
 # https://adventofcode.com/2019/day/1
 # my ans: 3454026
 
 # find fuel required given mass of each module, with formula floor(mass / 3) - 2
-
+puts "Part One:"
 puts File.open("input.txt").readlines.map(&:to_i).inject(0) { |sum, mass| sum + (mass/3) - 2}
+
+# part two
+# https://adventofcode.com/2019/day/1#part2
+# my ans: 5178170
+
+# include the mass of the fuel itself in the calculation
+puts "Part Two:"
+
+def getFuel(mass)
+	sum = 0
+	while mass > 8
+		sum += mass = (mass/3) - 2
+	end
+	return sum
+end
+
+puts File.open("input.txt").readlines.map(&:to_i).inject(0) { |sum, mass| sum + getFuel(mass)}
